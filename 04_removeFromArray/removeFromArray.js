8d56ae53c05fe3d35d125063391b93ba58c994c6
Raymond Bellin <raymondbellin346@gmail.com>
2022-12-21 07:12:31 -0600

    04_removeFromArray finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...itemsToRemove) {
+  for (const item of itemsToRemove) {
+    if (array.includes(item)) {
+      const index = array.indexOf(item);
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
