5d96a3a65966183308f1ede8acfa40e6e9257add
Raymond Bellin <raymondbellin346@gmail.com>
2022-12-20 19:28:20 -0600

    02_repeatString finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (word, number) {
+  if (number < 0) return "ERROR";
+  console.log(number);
+  let result = "";
+  for (let i = 0; i < number; i++) {
+    result = result.concat(word);
+  }
+  return result;
 };
 
 // Do not edit below this line
