d887524030bf60d533d273ef67ed9c86791e3a71
Raymond Bellin <raymondbellin346@gmail.com>
2022-12-21 11:35:59 -0600

    05_sumAll finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+  if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+  if (int1 < 0 || int2 < 0) return "ERROR";
+  let min = Math.min(int1, int2);
+  let max = Math.max(int1, int2);
+  let sum = 0;
+  for (let i = min; i < max + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
