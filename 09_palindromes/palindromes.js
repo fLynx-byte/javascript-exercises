fa8f8b33c238e28d24ec50846d269955e1aa3bcb
Raymond Bellin <raymondbellin346@gmail.com>
2023-01-10 19:08:53 -0600

    finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // Step 1: Clean the string by removing all non-alphanumeric characters and converting it to lowercase
+  var cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  // Step 2: Reverse the cleaned string
+  var reverseStr = cleanStr.split("").reverse().join("");
+  // Step 3: Check if the reversed string is the same as the cleaned string
+  return cleanStr === reverseStr;
 };
 
 // Do not edit below this line
